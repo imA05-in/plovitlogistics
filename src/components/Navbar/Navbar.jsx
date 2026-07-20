e0fa864118a2df2efbda7e624069bf08210f0e45
Abhishek <abhishek29112005@gmail.com>
2026-07-20 16:44:08 +0530

    Add Navbar Component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+export default function Navbar(){
+
+    const navItems = [
+        {
+            label : "About Us",
+            path: ""
+        },
+        {
+            label : "Our Services",
+            path: ""
+        },
+        {
+            label : "Contact Us",
+            path: ""
+        },
+    ]
+
+    return(
+        <header>
+            <div>
+                logo
+            </div>
+            <div>
+
+            </div>
+        </header>
+    )
+}
