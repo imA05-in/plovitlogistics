291807fbff72b5389c294773bca2964bf86f70d2
Abhishek <abhishek29112005@gmail.com>
2026-07-21 14:28:32 +0530

    import and export Add ContactUs and AboutUs Component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,7 @@ import OurServices from "./OurServices/OurServices"
 import ServiceCard from "./OurServices/ServiceCard"
 import AnchorBtn from "./Button/AnchorBtn"
 import Servidescripton from "./OurServices/ServiceDescription"
+import AboutUs from "./AboutUs/AboutUs"
+import ContactUs from "./ContactUs/ContactUs"
 
-export {Navbar, OurServices, ServiceCard, AnchorBtn, Servidescripton} 
+export {Navbar, OurServices, ServiceCard, AnchorBtn, Servidescripton, AboutUs, ContactUs} 
