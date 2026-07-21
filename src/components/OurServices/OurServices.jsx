5e30f1b74de7aea022345679683f01c20c2c991c
Abhishek <abhishek29112005@gmail.com>
2026-07-21 11:56:32 +0530

    add OurService layout

--- src/components/OurServices/OurServices.jsx
+++ src/components/OurServices/OurServices.jsx
@@ -1,7 +1,11 @@
 export default function OurServices(){
     return(
-        <div>
-            OurServices
+        <div className="flex min-h-screen gap-2">
+            <div className="border w-1/3">div 1</div>
+
+
+
+            <div className="border w-2/3">div 2</div>
         </div>
     )
 }
