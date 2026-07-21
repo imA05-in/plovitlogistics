9c8a6434f6ba86696d17c90f46691549e6428671
Abhishek <abhishek29112005@gmail.com>
2026-07-21 14:31:02 +0530

    configure router

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {createBrowserRouter, RouterProvider} from "react-router"
-import { OurServices } from './components/index.js'
+import { AboutUs, ContactUs, OurServices } from './components/index.js'
 
 const router = createBrowserRouter([
   {
@@ -13,6 +13,14 @@ const router = createBrowserRouter([
       {
         path: "/our-services",
         element:<OurServices/>
+      },
+      {
+        path:"about-us",
+        element: <AboutUs/>
+      },
+      {
+        path:"contact-us",
+        element:<ContactUs/>
       }
     ]
   }
