9725680dd4210fbe72e727e4bc4eabe3f5202212
Abhishek <abhishek29112005@gmail.com>
2026-07-21 13:28:34 +0530

    add ServiceCard Component

--- src/components/OurServices/ServiceCard.jsx
+++ src/components/OurServices/ServiceCard.jsx
@@ -1,7 +1,16 @@
-export default function ServiceCard(){
-    return(
-        <div>
-            SC
-        </div>
-    )
+import freightImg from "../../assets/DefaultImages/freight.png";
+
+export default function ServiceCard({
+  image = freightImg,
+  label = "default",
+  highlights,
+}) {
+  return (
+    <div>
+      <div className="p-2 ">
+        <img src={image} alt=""  className="rounded"/>
+      <p className="relative bottom-10 text-center font-bold text-xl bg-orange-400/70 text-blue-950">{label.toUpperCase()}</p>
+      </div>
+    </div>
+  );
 }
