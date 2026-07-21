eb096540e1bfb7240268ea8d2a69f30598728b7d
Abhishek <abhishek29112005@gmail.com>
2026-07-21 14:25:14 +0530

    import and export ServiceDescription Component

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Navbar from "../components/Navbar/Navbar"
 import OurServices from "./OurServices/OurServices"
 import ServiceCard from "./OurServices/ServiceCard"
 import AnchorBtn from "./Button/AnchorBtn"
+import Servidescripton from "./OurServices/ServiceDescription"
 
-export {Navbar, OurServices, ServiceCard, AnchorBtn} 
+export {Navbar, OurServices, ServiceCard, AnchorBtn, Servidescripton} 
