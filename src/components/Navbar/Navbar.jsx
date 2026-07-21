a57a9e49f80e4f2e3686d8d2a61d5aa59c97b279
Abhishek <abhishek29112005@gmail.com>
2026-07-21 11:43:30 +0530

    add Navbar routing mockup

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,40 @@
+import { NavLink } from "react-router"
+import logo from "../../assets/logo/logo.png"
+
 export default function Navbar(){
 
     const navItems = [
         {
             label : "About Us",
-            path: ""
+            path: "/"
         },
         {
             label : "Our Services",
-            path: ""
+            path: "2"
         },
         {
             label : "Contact Us",
-            path: ""
+            path: "3"
         },
     ]
 
     return(
-        <header>
-            <div>
-                logo
-            </div>
-            <div>
-
+        <header className="border flex justify-between items-center px-2 sticky top-0">
+            <NavLink className=""
+            to={`/`}
+            >
+                <img src={logo} alt="" className="h-20"/>
+            </NavLink>
+            <div className="flex gap-15">
+                {navItems.map((item)=>(
+                    <NavLink 
+                    key={item.label}
+                    to={item.path}
+                    className={({isActive})=>`${isActive ? "border-b" : ""}`}
+                    >
+                        {item.label}
+                    </NavLink>
+                ))}
             </div>
         </header>
     )
